0007dc4529fd49033ab1a290c07b8d7bfe08ee45
Pryscila Martins <103007680+PryscilaMartins@users.noreply.github.com>
2023-04-17 20:15:14 -0300

    Pacote facade e test java

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,8 +1,15 @@
 package one.digitalinnovation.gof;
 
+
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
 
 public class Test {
 
